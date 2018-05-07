860f2720dc71e366494a4e7ad5365422204af212
DavidHalkin <northfire@mail.ru>
2018-05-08 01:56:25 +0300

    ))

---
+++ app/js/main.js
@@ -0,0 +1,18 @@
+// custom select///
+$(function() {
+    jcf.replaceAll();
+});
+$(document).ready(function(){
+
+    $('.review_js').owlCarousel({
+        margin:20,
+        nav:true,
+        items:2
+    })
+    $('.yet_js').owlCarousel({
+        margin:18,
+        nav:true,
+        items:3
+    })
+    
+});
